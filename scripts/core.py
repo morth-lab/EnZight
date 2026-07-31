f2ce4cf7523a6937d344cbf9f3a0abdeb2f5552e
runrah <runrah@dtu.dk>
2026-07-31 14:10:26 +0200

    Update

--- scripts/core.py
+++ scripts/core.py
@@ -99,6 +99,6 @@ def EnZight(query, job_key, result_dir, tmp_dir="tmp", homologs=None, homology_s
         select_hotspots_in_pymol(single_hotspots, structures, align, cmd, mode=1)
         select_hotspots_in_pymol(double_hotspots, structures, align, cmd, mode=2)
 
-        cmd.util.cnc()
+        cmd.util.cnc(_self=cmd)
         cmd.save(os.path.join(result_dir,"EnZight_"+job_key+".pse"))
  
